f6a29900a19e778bd8a528d9e9794a60c5aba642
Kush <kushal@campion.ie>
2020-08-13 21:01:44 +0100

    Minimum in Circular Array

--- AugustCoding/src/MinAdjInCircularArray.java
+++ AugustCoding/src/MinAdjInCircularArray.java
@@ -1,31 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MinAdjInCircularArray {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        int arr[] = {8,-8,9,-9,10,-11,12};
+//        int arr[] = {8,-8,9,-9,10,-11,12};
 
-        int x = minAdjDiff(arr, arr.length);
-        System.out.println(x);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCases = Integer.parseInt(br.readLine());
+
+        while (testCases-- > 0) {
+            int sizeOfArray = Integer.parseInt(br.readLine());
+
+            int arr[] = new int[sizeOfArray];
+            String line = br.readLine();
+            String[] elements = line.trim().split("\\s+");
+
+            for (int i=0; i<sizeOfArray; i++) {
+                arr[i] = Integer.parseInt(elements[i]);
+            }
+
+            int x = minAdjDiff(arr, arr.length);
+            System.out.println(x);
+        }
     }
 
     public static int minAdjDiff(int arr[], int n) {
 
-        int result = 0;
-        // Your code here
-        int max = Integer.MIN_VALUE;
-        for (int i=0; i<arr.length - 2; i++) {
-            if (arr[i] < 0) {
-                arr[i] = arr[i] * -1;
-            } else if (arr[i+1] < 0) {
-                arr[i+1] = arr[i+1] * -1;
-            }
-            int addingNextValue = arr[i] + arr[i+1];
-            if (addingNextValue > max || addingNextValue < result) {
-                result = addingNextValue;
-                max = addingNextValue;
-            }
-        }
+       int result = 0;
+       result = Math.abs(arr[0] - arr[1]);
 
-        return result;
+       for (int i=2; i<n; i++) {
+           result = Math.min(result, Math.abs(arr[i] - arr[i-1]));
+       }
+
+       result = Math.min(result, Math.abs(arr[0] - arr[n - 1]));
+       return result;
     }
 }
