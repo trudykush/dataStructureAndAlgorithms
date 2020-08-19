f5ce0d45d969bfc5e5fffe33dbb307ea1dc4230f
Kush <kushal@campion.ie>
2020-08-19 21:59:16 +0100

    Smallest positive Missing Number in array

---
+++ AugustCoding/src/SmallestPositiveMissingNumberInArray.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SmallestPositiveMissingNumberInArray {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int numberOfCases = Integer.parseInt(br.readLine().trim());
+
+        while(numberOfCases-- > 0) {
+
+            int lengthOfArray = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[lengthOfArray];
+
+            String[] arrayElements = br.readLine().trim().split("\\s+");
+
+            for (int i = 0; i < lengthOfArray; i++) {
+                arr[i] = Integer.parseInt(arrayElements[i]);
+            }
+
+            int result = missingNumber(arr, lengthOfArray);
+        }
+    }
+
+    // TODO: Wrong solution below
+    private static int missingNumber(int[] A, int n) {
+
+        // To mark the occurrence of elements
+        boolean[] present = new boolean[n + 1];
+
+        // Mark the occurrences
+        for (int i = 0; i < n; i++) {
+
+            // Only mark the required elements
+            // All non-positive elements and
+            // the elements greater n + 1 will never
+            // be the answer
+            // For example, the array will be {1, 2, 3}
+            // in the worst case and the result
+            // will be 4 which is n + 1
+            if (A[i] > 0 && A[i] <= n)
+                present[A[i]] = true;
+        }
+
+        // Find the first element which didn't
+        // appear in the original array
+        for (int i = 1; i <= n; i++)
+            if (!present[i])
+                return i;
+
+        // If the original array was of the
+        // type {1, 2, 3} in its sorted form
+        return n + 1;
+    }
+}
