accc8e55141eeb9c9c0be6458d8b10f30503888b
Kush <kushal@campion.ie>
2020-08-20 22:21:37 +0100

    Maximum difference between Indexes for their values

---
+++ AugustCoding/src/MaximumIndexDifference.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MaximumIndexDifference {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int numberOfCases = Integer.parseInt(br.readLine().trim());
+
+        while(numberOfCases-- > 0) {
+
+            int lengthOfArray = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[lengthOfArray];
+
+            String[] arrayElements = br.readLine().trim().split("\\s+");
+
+            for (int i = 0; i < lengthOfArray; i++) {
+                arr[i] = Integer.parseInt(arrayElements[i]);
+            }
+
+            maxIndexDiff(arr, lengthOfArray);
+        }
+    }
+
+    private static int maxIndexDiff(int[] arr, int n) {
+
+        if (n == 1) {
+          return 0;
+        }
+
+        int RMax[] = new int[n];
+        int LMin[] = new int[n];
+
+        LMin[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            LMin[i] = Integer.min(arr[i], LMin[i - 1]);
+        }
+
+        /* Construct RMax[] such that RMax[j] stores the maximum value
+            from (arr[j], arr[j+1], ..arr[n-1]) */
+        RMax[n - 1] = arr[n - 1];
+        for (int j = n - 2; j >= 0; --j)
+            RMax[j] = Integer.max(arr[j], RMax[j + 1]);
+
+        /* Traverse both arrays from left to right to find optimum j - i
+            This process is similar to merge() of MergeSort */
+        int i = 0, j = 0, maxDiff = -1;
+        while (j < n && i < n) {
+            if (LMin[i] <= RMax[j]) {
+                maxDiff = Integer.max(maxDiff, j - i);
+                j++;
+            } else
+                i++;
+        }
+        return maxDiff;
+    }
+}
