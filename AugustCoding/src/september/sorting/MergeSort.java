647f1e4cd5603e067e9bb5f3a4086ef64aa24903
trudykush <kushalmishra91@gmail.com>
2020-10-04 00:12:24 +0100

    Merge Sort

---
+++ AugustCoding/src/september/sorting/MergeSort.java
@@ -0,0 +1,82 @@
+package september.sorting;
+
+import java.util.Scanner;
+
+public class MergeSort {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+
+        while(numberOfCases-- > 0) {
+            int lengthOfArray = sc.nextInt();
+            int[] arr = new int[lengthOfArray];
+
+            for (int i = 0; i < lengthOfArray; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            mergeSort(arr, 0, lengthOfArray-1);
+            printResult(arr);
+        }
+    }
+
+    private static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right-left)/2;
+
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid+1, right);
+
+            mergingSortedArray(arr, left, mid, right);
+        }
+    }
+
+    private static void mergingSortedArray(int[] arr, int left, int mid, int right) {
+
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            L[i] = arr[left + i];
+        }
+        for (int i = 0; i < n2; i++) {
+            R[i] = arr[mid + 1 + i];
+        }
+
+        int i = 0, j = 0;
+        int k = left;
+
+        while(i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i < n1) {
+            arr[k++] = L[i++];
+        }
+
+        while(j < n2) {
+            arr[k++] = R[j++];
+        }
+
+
+    }
+
+    private static void printResult(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
