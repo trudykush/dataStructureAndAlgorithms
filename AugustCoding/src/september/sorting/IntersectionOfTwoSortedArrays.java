4f34c1f75bb12f546c06933e0edbfde70d73b860
trudykush <kushalmishra91@gmail.com>
2020-10-20 21:44:01 +0100

    Intersection Of Two Sorted Arrays

--- AugustCoding/src/september/sorting/IntersectionOfTwoSortedArrays.java
+++ AugustCoding/src/september/sorting/IntersectionOfTwoSortedArrays.java
@@ -3,13 +3,13 @@ package september.sorting;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class UnionOfTwoSortedArrays {
+public class IntersectionOfTwoSortedArrays {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int numberOfCases = sc.nextInt();
 
-        while(numberOfCases-- > 0) {
+        while (numberOfCases-- > 0) {
             int lengthOfArrayOne = sc.nextInt();
             int[] arr1 = new int[lengthOfArrayOne];
 
@@ -24,51 +24,44 @@ public class UnionOfTwoSortedArrays {
                 arr2[i] = sc.nextInt();
             }
 
-            ArrayList<Integer> result = unionOfArrays(arr1, arr2, lengthOfArrayOne, lengthOfArrayTwo);
-            System.out.println(result);
+            ArrayList<Integer> result = intersectingBothArrays(arr1, arr2, lengthOfArrayOne, lengthOfArrayTwo);
+            printingResult(result);
         }
+
+
     }
 
-    private static ArrayList<Integer> unionOfArrays(int[] arr1, int[] arr2, int n1, int n2) {
+    private static void printingResult(ArrayList<Integer> result) {
+        for (int i = 0; i < result.size(); i++) {
+            System.out.print(result.get(i) + " ");
+        }
+        System.out.println();
+    }
+
+    private static ArrayList<Integer> intersectingBothArrays(int[] arr1, int[] arr2, int n1, int n2) {
 
         int i = 0, j = 0;
         ArrayList<Integer> result = new ArrayList<>();
+
         while (i<n1 && j<n2) {
 
-            while(i+1<n1 && arr1[i]==arr1[i+1]) {
+            while (i + 1 < n1 && arr1[i] == arr1[i + 1]) {
                 i++;
             }
 
-            while(j+1<n2 && arr2[j]==arr2[j+1]) {
+            while (j + 1 < n2 && arr2[j] == arr2[j + 1]) {
                 j++;
             }
 
-            if(arr1[i] < arr2[j]) {
-                result.add(arr1[i]);
+            if (arr1[i] < arr2[j]) {
                 i++;
             } else if (arr1[i] > arr2[j]) {
-                result.add(arr2[j]);
                 j++;
-            }
-            else if (arr1[i] == arr2[j]) {
+            } else if (arr1[i] == arr2[j]) {
                 result.add(arr1[i]);
                 i++;
                 j++;
             }
-        }
-
-        while (i<n1) {
-            while (i+1<n1 && arr1[i]==arr1[i+1]) {
-                i++;
-            }
-            result.add(arr1[i++]);
-        }
-
-        while (j<n2) {
-            while(j+1<n2 && arr2[j]==arr2[j+1]) {
-                j++;
-            }
-            result.add(arr2[j++]);
         }
         return result;
     }
