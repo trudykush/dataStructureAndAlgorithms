628bde18d6261b14bd99ef623fe870b0fb6a0510
trudykush <kushalmishra91@gmail.com>
2020-11-01 21:58:10 +0000

    Printing the output

--- AugustCoding/src/october/sorting/SortByAbsoluteDifference.java
+++ AugustCoding/src/october/sorting/SortByAbsoluteDifference.java
@@ -52,5 +52,10 @@ public class SortByAbsoluteDifference {
                 arr[index++] = al.get(i);
             }
         }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 }
