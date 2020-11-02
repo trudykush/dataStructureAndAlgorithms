0e005615d2f5166d65ca15ab4427ab0045c9f094
trudykush <kushalmishra91@gmail.com>
2020-11-02 21:49:51 +0000

    Finding Min Swap to sort

---
+++ AugustCoding/src/october/sorting/MinimumSwapToSort.java
@@ -0,0 +1,84 @@
+package october.sorting;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class MinimumSwapToSort {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+
+        while (numberOfCases-- > 0) {
+            int lengthOfArray = sc.nextInt();
+            int[] arr = new int[lengthOfArray];
+
+            for (int i = 0; i < lengthOfArray; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            /* Works but buggy*/
+            // int numberOfSwaps = findingMinimumSwapToSort(arr, lengthOfArray);
+
+            int numberOfSwaps = findingMinSwap(arr, lengthOfArray);
+        }
+    }
+
+    private static int findingMinSwap(int[] arr, int n) {
+
+        int ans = 0;
+        int[] temp = Arrays.copyOfRange(arr, 0, n);
+
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        Arrays.sort(temp);
+
+        for (int i = 0; i < n; i++) {
+            hashMap.put(arr[i], i);
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != temp[i]) {
+
+                ans++;
+                int init = arr[i];
+                swap(arr, i, hashMap.get(temp[i]));
+
+                hashMap.put(init, hashMap.get(temp[i]));
+                hashMap.put(temp[i], i);
+            }
+        }
+
+        return ans;
+    }
+
+    private static void swap(int[] arr, int i, Integer j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    private static int findingMinimumSwapToSort(int[] arr, int n) {
+
+        int numberOfSwaps = 0;
+        for (int i = 0; i < n; i++) {
+            int j = i+1, r = n-1;
+
+            while (j < r) {
+                if (arr[j] > arr[r]) {
+                    int temp = arr[j];
+                    arr[j] = arr[r];
+                    arr[r] = temp;
+                    j++;
+                    r--;
+                    numberOfSwaps++;
+                } else {
+                    j++;
+                    r--;
+                }
+            }
+        }
+        return numberOfSwaps;
+    }
+}
