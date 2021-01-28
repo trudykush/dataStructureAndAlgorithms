06a60b9034d906105231dad12a0e645c14f38767
trudykush <kushalmishra91@gmail.com>
2021-01-28 23:51:59 +0000

    Base input method

--- Jan2021/src/matrix/UpperAndLowerTriangleSum.java
+++ Jan2021/src/matrix/UpperAndLowerTriangleSum.java
@@ -1,4 +1,27 @@
 package matrix;
 
+import java.util.Scanner;
+
 public class UpperAndLowerTriangleSum {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+
+        while (numberOfCases-- > 0) {
+            int m1 = sc.nextInt();
+            int n1 = sc.nextInt();
+            int[][] matrix = new int[m1][n1];
+
+            for (int i = 0; i < m1; i++) {      // Looping through Row
+                for (int j = 0; j < n1; j++) {  // Looping through Column
+                    matrix[i][j] = sc.nextInt();
+                }
+            }
+
+            //sumOfUpperAndLowerTriangle(matrix, m1, n1);
+        }
+    }
+
+
 }
