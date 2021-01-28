3af62fa15f9297bfa216627b57566522a9857903
trudykush <kushalmishra91@gmail.com>
2021-01-28 23:52:36 +0000

    calculation method - upper and lower triangle sum

--- Jan2021/src/matrix/UpperAndLowerTriangleSum.java
+++ Jan2021/src/matrix/UpperAndLowerTriangleSum.java
@@ -19,9 +19,35 @@ public class UpperAndLowerTriangleSum {
                 }
             }
 
-            //sumOfUpperAndLowerTriangle(matrix, m1, n1);
+            sumOfUpperAndLowerTriangle(matrix, m1, n1);
         }
     }
 
+    private static void sumOfUpperAndLowerTriangle(int[][] matrix, int m1, int n1) {
 
+        int upper_sum = 0;
+        int lower_sum = 0;
+
+        // Upper Triangle Sum
+        for (int i = 0; i < m1; i++) {
+            for (int j = 0; j < n1; j++) {
+                if(i <= j) {
+                    upper_sum += matrix[i][j];
+                }
+            }
+        }
+
+        // Lower Triangle Sum
+        for (int i = 0; i < m1; i++) {
+            for (int j = 0; j < n1; j++) {
+                if (i >= j) {
+                    lower_sum += matrix[i][j];
+                }
+            }
+        }
+
+        System.out.println("The sum of upper triangle is: " + upper_sum);
+        System.out.println("The sum of lower triangle is: " + lower_sum);
+
+    }
 }
