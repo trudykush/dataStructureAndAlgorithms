51cce6c53ce76a3e3f7189dc8074a4b6cdf10ddd
trudykush <kushalmishra91@gmail.com>
2021-01-29 22:59:16 +0000

    MultipleMatrices method

--- Jan2021/src/matrix/MultipleMatrices.java
+++ Jan2021/src/matrix/MultipleMatrices.java
@@ -29,12 +29,37 @@ public class MultipleMatrices {
                 }
             }
 
-            multiplyingMatrices(firstMatrix, m1, n1,
-                                secondMatrix, m2, n2);
+            multiplyingMatrices(firstMatrix, secondMatrix);
         }
     }
 
-    private static void multiplyingMatrices(int[][] firstMatrix, int m1, int n1, int[][] secondMatrix, int m2, int n2) {
-        
+    private static int[][] multiplyingMatrices(int[][] A, int[][] B) {
+
+        int n1 = A.length;
+        int m1 = A[0].length;
+        int n2 = B.length;
+        int m2 = B[0].length;
+
+        if(m1 != n2)
+        {
+            return new int[0][0];
+        }
+        else
+        {
+            int[][] result = new int[n1][m2];
+            for(int i = 0; i  < n1; i++)
+            {
+                for(int j = 0; j < m2; j++)
+                {
+                    int sum=0;
+                    for(int k=0; k<m1; k++)
+                    {
+                        sum += A[i][k] * B[k][j];
+                    }
+                    result[i][j] = sum;
+                }
+            }
+            return result;
+        }
     }
 }
