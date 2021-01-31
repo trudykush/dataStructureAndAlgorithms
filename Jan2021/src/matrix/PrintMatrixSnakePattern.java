ea7c609def84a0e5e236913dca551f025674f44e
trudykush <kushalmishra91@gmail.com>
2021-01-31 22:44:52 +0000

    snake pattern - user input

--- Jan2021/src/matrix/PrintMatrixSnakePattern.java
+++ Jan2021/src/matrix/PrintMatrixSnakePattern.java
@@ -1,4 +1,26 @@
 package matrix;
 
+import java.util.Scanner;
+
 public class PrintMatrixSnakePattern {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+
+        while (numberOfCases-- > 0) {
+            int m1 = sc.nextInt();
+            int n1 = sc.nextInt();
+            int[][] matrix = new int[m1][n1];
+
+            for (int i = 0; i < m1; i++) {
+                for (int j = 0; j < n1; j++) {
+                    matrix[i][j] = sc.nextInt();
+                }
+            }
+
+            snakePattern(matrix, m1, n1);
+        }
+    }
+
 }
