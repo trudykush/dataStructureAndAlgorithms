ffd3f65555c40cce0e5f2b2b514ffffe3961cdbc
Kush <kushalmishra91@gmail.com>
2021-09-07 21:26:51 +0100

    interface Coach

--- LearningSpringOne/src/com/kush/springdemo/BaseballCoach.java
+++ LearningSpringOne/src/com/kush/springdemo/BaseballCoach.java
@@ -1,7 +1,8 @@
 package com.kush.springdemo;
 
-public class BaseballCoach {
+public class BaseballCoach implements Coach {
 
+	@Override
 	public String getDailyWorkout() {
 		return "Spend 30 mins in batting practice";
 	}
---
+++ LearningSpringOne/src/com/kush/springdemo/Coach.java
@@ -0,0 +1,6 @@
+package com.kush.springdemo;
+
+public interface Coach {
+
+	public String getDailyWorkout();
+}
