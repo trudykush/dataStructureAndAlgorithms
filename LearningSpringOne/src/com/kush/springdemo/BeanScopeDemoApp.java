bb79563e838eccc5e2014a07d4ae5d01a807c0b4
Kush <kushalmishra91@gmail.com>
2021-09-08 23:21:15 +0100

    singleton bean

--- LearningSpringOne/src/com/kush/springdemo/BeanScopeDemoApp.java
+++ LearningSpringOne/src/com/kush/springdemo/BeanScopeDemoApp.java
@@ -1,10 +1,31 @@
 package com.kush.springdemo;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class BeanScopeDemoApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// load the spring config file
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+		
+		// retrieve bean from spring container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		
+		Coach alphaCoach = context.getBean("myCoach", Coach.class);
+		
+		// check if they are the same 
+		boolean result = (theCoach == alphaCoach);
+		
+		System.out.println(result);
+		
+		System.out.println("Memory: " + theCoach);
+		
+		System.out.println("Memory: " + alphaCoach);
+		
+		// close the context
+		context.close();
 	}
 
 }
