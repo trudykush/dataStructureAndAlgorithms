fed88f08391d8130379d95299e58d9c0a05e7808
Kush <kushalmishra91@gmail.com>
2021-09-15 22:20:31 +0100

    bean

--- spring-demo-annotations/src/com/kush/springdemo/AnnotationBeanScopDemoApp.java
+++ spring-demo-annotations/src/com/kush/springdemo/AnnotationBeanScopDemoApp.java
@@ -1,12 +1,19 @@
 package com.kush.springdemo;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class AnnotationBeanScopDemoApp {
 
 	public static void main(String[] args) {
 	
 		// load spring config file
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicatinoContext.xml");
 		
 		// retrieve bean from spring container
+		Coach theCoach = 
+				context.getBean("tennisCoach", Coach.class);
+		
 
 	}
 
