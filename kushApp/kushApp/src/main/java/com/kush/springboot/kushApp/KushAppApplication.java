94c21d9b09ca963fd05cbc7138479d213a4e5daa
Kush <kushalmishra91@gmail.com>
2021-09-20 22:39:13 +0100

    Spring boot - kushApp

---
+++ kushApp/kushApp/src/main/java/com/kush/springboot/kushApp/KushAppApplication.java
@@ -0,0 +1,13 @@
+package com.kush.springboot.kushApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KushAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KushAppApplication.class, args);
+	}
+
+}
---
+++ kushApp/kushApp/src/test/java/com/kush/springboot/kushApp/KushAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kush.springboot.kushApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KushAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
